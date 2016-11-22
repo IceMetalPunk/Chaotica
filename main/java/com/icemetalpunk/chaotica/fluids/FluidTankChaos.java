4f91ee9076925d6c060ffc0fb55b4580b8d2ebcc
Kevin <IceMetalPunk@gmail.com>
2016-11-22 18:43:30 -0500

    Moved tank rendering into tank object.

--- main/java/com/icemetalpunk/chaotica/fluids/FluidTankChaos.java
+++ main/java/com/icemetalpunk/chaotica/fluids/FluidTankChaos.java
@@ -1,5 +1,8 @@
 package com.icemetalpunk.chaotica.fluids;
 
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.FluidTank;
 
@@ -7,6 +10,28 @@ public class FluidTankChaos extends FluidTank {
 
 	public FluidTankChaos(int capacity) {
 		super(capacity);
+	}
+
+	// Render the fluid tank to a GUI.
+	public void render(GuiScreen gui, int left, int bottom, int width, int height) {
+		int right = left + width;
+		int top = bottom - height;
+
+		// Get fluid display data
+		int color = 0xFFFFFFFF, amount = 0;
+		Fluid fluid = this.getFluid().getFluid();
+		if (fluid != null) {
+			color = fluid.getColor();
+			amount = this.getFluidAmount();
+		}
+		top = bottom - (int) (height * (double) amount / (double) this.getCapacity());
+
+		// Draw fluid tank filling. Needs to be scaled down by 0.5 for some
+		// weird reason or else scaling is all off.
+		GlStateManager.pushMatrix();
+		GlStateManager.scale(0.5f, 0.5f, 0.5f);
+		gui.drawRect(left, top, right, bottom, color);
+		GlStateManager.popMatrix();
 	}
 
 	@Override
